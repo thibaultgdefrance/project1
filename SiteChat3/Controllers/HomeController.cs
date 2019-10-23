d6231240392e411c60733609a1da5567cb512901
Thibault Defrance <thibault.g.defrance@gmail.com>
2019-10-23 20:24:29 +0200

    new

---
+++ SiteChat3/Controllers/HomeController.cs
@@ -0,0 +1,109 @@
+﻿using SiteChat3.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SiteChat3.Controllers
+{
+    public class HomeController : Controller
+    {
+        Chat2Entities db = new Chat2Entities();
+        Workflow workflow = new Workflow();
+        public ActionResult Index()
+        {
+            Utilisateur utilisateur = new Utilisateur();
+           
+
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+
+        [HttpPost]
+        public ActionResult Connexion(FormCollection collection)
+        {
+            ViewBag.messageErreure = "";
+            var MailUtilisateur = collection["mail"];
+            var MDPUtilisateur = collection["MDP"];
+            int utilisateurExiste = (from u in db.Utilisateur where u.EmailUtilisateur == MailUtilisateur && u.MotDePasseUtilisateur == MDPUtilisateur select u).Count();
+            if (utilisateurExiste > 0)
+            {
+                return RedirectToAction("MessagesApi", "Messages");
+            }
+            else
+            {
+                ViewBag.messageErreure = "email et/ou mot de passe incorrecte(s)";
+                return RedirectToAction("Index", "Home");
+
+            }
+
+
+        }
+
+
+        [HttpPost]
+        public ActionResult Inscription(FormCollection collection)
+        {
+            ViewBag.messageErreure = "";
+            var MailUtilisateur = collection["mail"];
+            var MDPUtilisateur = collection["MDP"];
+            int utilisateurExiste = (from u in db.Utilisateur where u.EmailUtilisateur == MailUtilisateur select u).Count();
+            if (utilisateurExiste > 0)
+            {
+                ViewBag.MessageErreure = "un compte associé à cette adresse mail existe déjà";
+                return RedirectToAction("Index", "Home");
+            }
+            else
+            {
+                ViewBag.messageErreure = "";
+                //Instanciation du client
+                SmtpClient smtpClient = new SmtpClient("webmaster@cucarachat.com", 25);
+                //On indique au client d'utiliser les informations qu'on va lui fournir
+                smtpClient.UseDefaultCredentials = true;
+                //Ajout des informations de connexion
+                smtpClient.Credentials = new System.Net.NetworkCredential("webmaster@cucarachat.com", "hq2y5877hsb6");
+                //On indique que l'on envoie le mail par le réseau
+                smtpClient.DeliveryMethod = SmtpDeliveryMethod.Network;
+                //On active le protocole SSL
+                smtpClient.EnableSsl = true;
+
+                MailMessage mail = new MailMessage();
+                //Expéditeur
+                mail.From = new MailAddress("webmaster@cucarachat.com", "Cucarachat");
+                //Destinataire
+                mail.To.Add(new MailAddress("thibaustin@hotmail.fr"));
+
+                mail.Subject = "lien de confirmation";
+                mail.Body = "dxfb1d35hb4fd54h53t4d35ffh3h3";
+                //Copie
+                //mail.CC.Add(new MailAddress("toto@gmail.com"));
+
+                smtpClient.Send(mail);
+                return RedirectToAction("Index","Home");
+
+            }
+
+
+        }
+
+        
+
+    }
+}
---
+++ SiteChat3/Controllers/UtilisateursController.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using SiteChat3.Models;
+
+namespace SiteChat3.Controllers
+{
+    public class UtilisateursController : Controller
+    {
+        private Chat2Entities db = new Chat2Entities();
+
+        // GET: Utilisateurs
+        public async Task<ActionResult> Index()
+        {
+            var utilisateur = db.Utilisateur.Include(u => u.Acces).Include(u => u.Avatar);
+            return View(await utilisateur.ToListAsync());
+        }
+
+        // GET: Utilisateurs/Details/5
+        public async Task<ActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Utilisateur utilisateur = await db.Utilisateur.FindAsync(id);
+            if (utilisateur == null)
+            {
+                return HttpNotFound();
+            }
+            return View(utilisateur);
+        }
+
+        // GET: Utilisateurs/Create
+        public ActionResult Create()
+        {
+            ViewBag.IdAcces = new SelectList(db.Acces, "IdAcces", "LibelleAcces");
+            ViewBag.IdAvatar = new SelectList(db.Avatar, "IdAvatar", "CheminAvatar");
+            return View();
+        }
+
+        // POST: Utilisateurs/Create
+        // Afin de déjouer les attaques par sur-validation, activez les propriétés spécifiques que vous voulez lier. Pour 
+        // plus de détails, voir  https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create([Bind(Include = "IdUtilisateur,NomUtilisateur,PrenomUtilisateur,PseudoUtilisateur,EmailUtilisateur,DateDeNaissanceUtilisateur,NumeroUtilisateur,MotDePasseUtilisateur,DateCreationUtilisateur,IdAvatar,IdAcces,StatutUtilisateur")] Utilisateur utilisateur)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Utilisateur.Add(utilisateur);
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.IdAcces = new SelectList(db.Acces, "IdAcces", "LibelleAcces", utilisateur.IdAcces);
+            ViewBag.IdAvatar = new SelectList(db.Avatar, "IdAvatar", "CheminAvatar", utilisateur.IdAvatar);
+            return View(utilisateur);
+        }
+
+        // GET: Utilisateurs/Edit/5
+        public async Task<ActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Utilisateur utilisateur = await db.Utilisateur.FindAsync(id);
+            if (utilisateur == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.IdAcces = new SelectList(db.Acces, "IdAcces", "LibelleAcces", utilisateur.IdAcces);
+            ViewBag.IdAvatar = new SelectList(db.Avatar, "IdAvatar", "CheminAvatar", utilisateur.IdAvatar);
+            return View(utilisateur);
+        }
+
+        // POST: Utilisateurs/Edit/5
+        // Afin de déjouer les attaques par sur-validation, activez les propriétés spécifiques que vous voulez lier. Pour 
+        // plus de détails, voir  https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit([Bind(Include = "IdUtilisateur,NomUtilisateur,PrenomUtilisateur,PseudoUtilisateur,EmailUtilisateur,DateDeNaissanceUtilisateur,NumeroUtilisateur,MotDePasseUtilisateur,DateCreationUtilisateur,IdAvatar,IdAcces,StatutUtilisateur")] Utilisateur utilisateur)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(utilisateur).State = EntityState.Modified;
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            ViewBag.IdAcces = new SelectList(db.Acces, "IdAcces", "LibelleAcces", utilisateur.IdAcces);
+            ViewBag.IdAvatar = new SelectList(db.Avatar, "IdAvatar", "CheminAvatar", utilisateur.IdAvatar);
+            return View(utilisateur);
+        }
+
+        // GET: Utilisateurs/Delete/5
+        public async Task<ActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Utilisateur utilisateur = await db.Utilisateur.FindAsync(id);
+            if (utilisateur == null)
+            {
+                return HttpNotFound();
+            }
+            return View(utilisateur);
+        }
+
+        // POST: Utilisateurs/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> DeleteConfirmed(int id)
+        {
+            Utilisateur utilisateur = await db.Utilisateur.FindAsync(id);
+            db.Utilisateur.Remove(utilisateur);
+            await db.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
